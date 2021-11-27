b5fb947e43f8d552fb66e46896c218505c118582
Navneet <navn0306@gmail.com>
2021-11-27 17:32:21 +0530

    Add: Added Welcome message

---
+++ src/com/bridge/LeapYear/LeapYear.java
@@ -0,0 +1,8 @@
+package com.bridge.LeapYear;
+
+public class LeapYear {
+
+    public static void main(String[] args) {
+        System.out.println(" \n Welcome, This is a program to check if a given number is Leap Year or not.");
+    }
+}
