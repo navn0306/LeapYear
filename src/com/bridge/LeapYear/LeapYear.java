953ec04531406a1ce7b2a06a985f3bbc230caf82
Navneet <navn0306@gmail.com>
2021-11-27 17:36:13 +0530

    ADD: Added program for checking if the entered Year is a Leap Year

--- src/com/bridge/LeapYear/LeapYear.java
+++ src/com/bridge/LeapYear/LeapYear.java
@@ -1,8 +1,20 @@
 package com.bridge.LeapYear;
 
+import java.util.Scanner;
+
 public class LeapYear {
 
     public static void main(String[] args) {
         System.out.println(" \n Welcome, This is a program to check if a given number is Leap Year or not.");
+        int year;
+        System.out.println("Enter an Year :: ");
+        Scanner sc = new Scanner(System.in);
+        year = sc.nextInt();
+
+        if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+            System.out.println("Specified year is a leap year");
+        else
+            System.out.println("Specified year is not a leap year");
     }
 }
+
